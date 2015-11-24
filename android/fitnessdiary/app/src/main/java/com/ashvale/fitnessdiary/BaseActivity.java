3e982faf6820502c64e2fde60e684ff15c3f68b5
Yuehyi <yuehyi@gmail.com>
2015-11-24 19:31:29 +0800

    android new project

---
+++ android/fitnessdiary/app/src/main/java/com/ashvale/fitnessdiary/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.ashvale.fitnessdiary;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class BaseActivity extends AppCompatActivity {
+    public DrawerLayout drawerLayout;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
